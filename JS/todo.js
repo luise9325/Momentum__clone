78a8b66d49d932e52662f5ee627c875db4a5b3cc
luise9325 <97442852+luise9325@users.noreply.github.com>
2023-01-29 22:11:03 +0900

    adding todo

--- JS/todo.js
+++ JS/todo.js
@@ -4,13 +4,17 @@ const todoList = document.querySelector(".todo_list");
 
 todoForm.addEventListener("submit", todoSubmit);
 
+function paintTodo(newTodo) {
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    todoList.appendChild(li);
+}
+
 function todoSubmit(event) {
     event.preventDefault();
     const newTodo = todoInput.value;
     todoInput.value = "";
     paintTodo(newTodo);
-}
-
-function paintTodo(newTodo) {
-
 }
